ec39a32197ab1ed96f1c08ca4f1c7ec6a5d16928
Cillian <l00162026@student.lyit.ie>
2021-01-25 22:17:34 +0000

    Commit to TestBranch2 with comment 3. on the main class

--- src/com/Cillian/Main.java
+++ src/com/Cillian/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //1. This comment was written on the local main branch
         //2. This comment was written on the local Test1 branch
+        //3. This comment was written on the local Test2 branch
 
     }
 }
