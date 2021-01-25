755e9df1912e729534d2caf5278fac086fd16915
Cillian <l00162026@student.lyit.ie>
2021-01-25 22:14:04 +0000

    Commit to TestBranch1 with comment 2. on the main class

--- src/com/Cillian/Main.java
+++ src/com/Cillian/Main.java
@@ -3,7 +3,8 @@ package com.Cillian;
 public class Main {
 
     public static void main(String[] args) {
-    //1. This comment was written on the local main branch
+        //1. This comment was written on the local main branch
+        //2. This comment was written on the local Test1 branch
 
     }
 }
