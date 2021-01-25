7996b6b0982ee40601cd477712d49cfd699341af
Cillian <l00162026@student.lyit.ie>
2021-01-25 22:24:48 +0000

    Comment 5 and 4(on TestBranch1) will be merged to main

--- src/com/Cillian/Main.java
+++ src/com/Cillian/Main.java
@@ -6,6 +6,7 @@ public class Main {
         //1. This comment was written on the local main branch
         //2. This comment was written on the local Test1 branch
         //3. This comment was written on the local Test2 branch
+        //5. This comment and comment 4 on TestBranch1 will be merged to main
 
     }
 }
