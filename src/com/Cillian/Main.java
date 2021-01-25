0cb7eb1e3feb96648e537f9cfdf120152e75dcc8
Cillian <l00162026@student.lyit.ie>
2021-01-25 22:11:27 +0000

    Commit with comment 1. on the main class

--- src/com/Cillian/Main.java
+++ src/com/Cillian/Main.java
@@ -3,6 +3,7 @@ package com.Cillian;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    //1. This comment was written on the local main branch
+
     }
 }
